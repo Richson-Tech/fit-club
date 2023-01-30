a4e371aa254f617af3bae3e2fcf6bcddbf853281
Folorunsho Ahmed <iamrichson2020@gmail.com>
2023-01-30 13:24:27 +0100

    second commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -17,6 +17,8 @@ const Footer = () => {
         </div>
         <div className="logo-f">
           <img src={Logo} alt="" />
+          <p className="tech">Copyright<sup >&copy;</sup> All Reserved Rights <span>RICHSON TECH. 2023</span></p>
+      
         </div>
         <div className="blur blur-f-1"></div>
         <div className="blur blur-f-2"></div>
